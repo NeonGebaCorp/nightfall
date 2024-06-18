16cbdc997cf6009c13c66dca5d19e2368bdf25c3
arkanataya <78206974+arkanataya@users.noreply.github.com>
2024-06-18 16:18:06 +0700

    Create nightfall.h

---
+++ nightfall.h
@@ -0,0 +1,14 @@
+#ifndef NIGHTFALL_H
+#define NIGHTFALL_H
+
+// Function prototypes
+void aprint(const char *message);
+void areboot();
+void apoweroff();
+void abootable();
+void asetgraphics(int width, int height);
+void acolor(const char *hex);
+void acli();
+void initfs();
+
+#endif // NIGHTFALL_H
